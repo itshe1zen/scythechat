e665af65c8c79290ddaaadab39385a2f050a8486
cipherbin <winrarmain@gmail.com>
2025-01-17 19:02:44 +0300

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-// Example script to dynamically change text in the text boxes
-document.getElementById('box1-text').textContent = 'DFS Animations.';
-document.getElementById('box2-text').textContent = 'Rebecca K. Lunetta.';
-document.getElementById('box3-text').textContent = 'New Fixed Text 3';
